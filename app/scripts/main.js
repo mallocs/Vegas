e78a1d88525e2b88224dbaa4a5ba75035f04bad4
Marcus Ulrich <ulrich@mallocs.net>
2015-06-04 13:11:22 -0700

    Updating

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,16 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+(function ( $ ) {
+    var MAP_ID = '#map-pane';
+    
+    $('.link-to-map').on('click', function() {
+        $('#vegas-tabs-box a[href="' + MAP_ID + '"]').tab('show');
+        window.location.hash = MAP_ID;
+    });
+    
+    var showmoreEl = $('<a>Show more</a>').on('click', function() {
+            alert('hi');
+        });
+    $('.showmore').after( showmoreEl );
+    
+})( jQuery );
